7637f5bb4dafd0b06ee32b58cea7ef3fd0f3106b
nouseph <61069607+nouseph@users.noreply.github.com>
2020-06-10 08:52:24 -0700

    Initial commit

---
+++ EventCatalogAPI/Domain/CatalogEventItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventCatalogAPI.Domain
+{
+    public class CatalogEventItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+        public string PictureUrl { get; set; }
+        public int CatalogEventCategoryId { get; set; }
+        public CatalogEventCategory CatalogEventCategory { get; set; }
+    }
+}
