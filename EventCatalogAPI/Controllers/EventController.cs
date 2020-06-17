345c84972e65ffd4869b26c522cc93a1c17e4d05
GessicaM <58050050+GessicaM@users.noreply.github.com>
2020-06-17 14:25:45 -0700

    Added piccontroller and eventcontroller functionality

---
+++ EventCatalogAPI/Controllers/EventController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EventCatalogAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventCatalogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        private readonly CatalogEventContext _context;
+
+        public EventController(CatalogEventContext context)
+
+        {
+
+            _context = context;
+
+        }
+
+
+
+        [HttpGet("[action]")]
+
+        public async Task<IActionResult> Items(
+
+            [FromQuery] int pageIndex = 0,
+
+            [FromQuery] int pageSize = 4)
+
+        {
+
+            var items = await _context.CatalogEventItems
+
+                            .OrderBy(c => c.Name)
+
+                            .Skip(pageIndex * pageSize)
+
+                            .Take(pageSize)
+
+                            .ToListAsync();
+
+
+
+            return Ok(items);
+
+        }
+    }
+}
