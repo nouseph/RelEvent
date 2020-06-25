2a254f3660ce7b57bfef57d444cad22e85338f28
Ahsan Farooq <ahsan.farooq@yahoo.com>
2020-06-24 18:34:37 -0700

    Completed items controller

--- EventCatalogAPI/Controllers/EventController.cs
+++ EventCatalogAPI/Controllers/EventController.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EventCatalogAPI.Data;
+using EventCatalogAPI.Domain;
+using EventCatalogAPI.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace EventCatalogAPI.Controllers
 {
@@ -14,41 +17,45 @@ namespace EventCatalogAPI.Controllers
     public class EventController : ControllerBase
     {
         private readonly CatalogEventContext _context;
-
-        public EventController(CatalogEventContext context)
+        private readonly IConfiguration _config;
+        public EventController(CatalogEventContext context, IConfiguration config)
 
         {
-
             _context = context;
-
+            _config = config;
         }
 
-
-
         [HttpGet("[action]")]
-
         public async Task<IActionResult> Items(
-
             [FromQuery] int pageIndex = 0,
-
             [FromQuery] int pageSize = 4)
-
         {
-
+            var itemsCount = await _context.CatalogEventItems.LongCountAsync();
             var items = await _context.CatalogEventItems
-
                             .OrderBy(c => c.Name)
-
                             .Skip(pageIndex * pageSize)
-
                             .Take(pageSize)
-
                             .ToListAsync();
 
+            items = ChangePictureUrl(items);
+            var model = new PaginatedItemsViewModel<CatalogEventItem>
+            {
+                PageIndex = pageIndex,
+                PageSize = items.Count,
+                Count = itemsCount,
+                Data = items
+            };
 
+            return Ok(model);
+        }
 
-            return Ok(items);
-
+        private List<CatalogEventItem> ChangePictureUrl(List<CatalogEventItem> items)
+        {
+            items.ForEach(item =>
+                item.PictureUrl = item.PictureUrl.Replace(
+                    "http://externalcatalogbaseurltobereplaced",
+                    _config["ExternalCatalogBaseUrl"]));
+            return items;
         }
     }
 }
