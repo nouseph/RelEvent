0d9ae6c1067bfb0c39ecbce12bfed2bc8e4a6651
nouseph <61069607+nouseph@users.noreply.github.com>
2020-07-18 10:38:55 -0700

    Added webmvc

---
+++ WebMvc/Controllers/CatalogController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebMvc.Services;
+using WebMvc.ViewModels;
+
+namespace WebMvc.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly ICatalogService _service;
+        public CatalogController(ICatalogService service)
+        {
+            _service = service;
+        }
+
+        public async Task<IActionResult> Index(int? page, int? categoryFilterApplied, int? typesFilterApplied)
+        {
+            var itemsOnPage = 5;
+
+            var catalog = await _service.GetCatalogItemsAsync(page ?? 0, itemsOnPage, categoryFilterApplied, typesFilterApplied);
+
+            var vm = new CatalogIndexViewModel
+            {
+                CatalogItems = catalog.Data,
+                Categories = await _service.GetCategoriesAsync(),
+                Types = await _service.GetTypesAsync(),
+                PaginationInfo = new PaginationInfo
+                {
+                    ActualPage = page ?? 0,
+                    ItemsPerPage = itemsOnPage,
+                    TotalItems = catalog.Count,
+                    TotalPages = (int)Math.Ceiling((decimal)catalog.Count / itemsOnPage)
+                },
+                CategoryFilterApplied = categoryFilterApplied ?? 0,
+                TypesFilterApplied = typesFilterApplied ?? 0
+            };
+
+            return View(vm);
+        }
+    }
+}
---
+++ WebMvc/Infrastructure/ApiPaths.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMvc.Infrastructure
+{
+    public static class ApiPaths
+    {
+        public static class Catalog
+        {
+            public static string GetAllTypes(string baseUri)
+            {
+                return $"{baseUri}catalogeventtypes";
+            }
+            public static string GetAllCategories(string baseUri)
+            {
+                return $"{baseUri}catalogeventcategories";
+            }
+            public static string GetAllCatalogItems(string baseUri, int page, int take, int? category, int? type)
+            {
+                var filterQs = string.Empty;
+                if (category.HasValue || type.HasValue)
+                {
+                    var categoryQs = (category.HasValue) ? category.Value.ToString() : " ";
+                    var typeQs = (type.HasValue) ? type.Value.ToString() : " ";
+                    filterQs = $"/type/{typeQs}/category/{categoryQs}";
+                }
+                return $"{baseUri}items{filterQs}?pageIndex={page}&pageSize={take}";
+            }
+        }
+    }
+
+
+
+}
+
